817756e33f95e6a2f7d0395095f25f9d7db94be2
Lucas Rafael Booz <67797271+lucasbooz@users.noreply.github.com>
2022-10-10 10:14:21 -0300

    Upload de arquivos da aula de banco de dados

---
+++ BancoDados/ProjetoSistemaCadastro/SlnSistemaCadastro/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Data/EnderecoRepository.cs
@@ -0,0 +1,74 @@
+﻿using Devs2Blu.ProjetosAula.SistemaCadastro.Models.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Devs2Blu.ProjetosAula.SistemaCadastro.Forms.Data
+{
+    public class EnderecoRepository
+    {
+
+        public static void SaveEndereco(Endereco endereco, MySqlConnection conn, Int32 idpessoa)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(SQL_INSERT_ENDERECO, conn);
+                cmd.Parameters.Add("@idPessoa", MySqlDbType.Int32).Value = idpessoa;
+                cmd.Parameters.Add("@CEP", MySqlDbType.VarChar, 50).Value = endereco.CEP;
+                cmd.Parameters.Add("@rua", MySqlDbType.VarChar, 25).Value = endereco.Rua;
+                cmd.Parameters.Add("@numero", MySqlDbType.Enum).Value = endereco.Numero;
+                cmd.Parameters.Add("@bairro", MySqlDbType.Enum).Value = endereco.Bairro;
+                cmd.Parameters.Add("@cidade", MySqlDbType.Enum).Value = endereco.Cidade;
+                cmd.Parameters.Add("@UF", MySqlDbType.Enum).Value = endereco.UF;
+
+                cmd.ExecuteNonQuery();            }
+            catch (MySqlException myExc)
+            {
+                MessageBox.Show(myExc.Message, "Erro de MySQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        internal MySqlDataReader GetEnderecos()
+        {
+            MySqlConnection conn = ConnectionMySQL.GetConnection();
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand(SQL_SELECT_ENDERECOS, conn);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                return dataReader;
+            }
+            catch (MySqlException myExc)
+            {
+                MessageBox.Show(myExc.Message, "Erro de MySQL", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+        }
+
+        #region SQLS
+        private const String SQL_INSERT_ENDERECO = @"INSERT INTO endereco
+(id_pessoa,
+CEP,
+rua,
+numero,
+bairro,
+cidade,
+uf)
+VALUES
+(@idPessoa,
+@CEP,
+@rua,
+@numero,
+@bairro,
+@cidade,
+@uf)";
+        private const String SQL_SELECT_ENDERECOS = @"SELECT id, id_pessoa, CEP, rua,numero, bairro, cidade, uf from endereco";
+        #endregion
+    }
+}
---
+++ BancoDados/ProjetoSistemaCadastro/SlnSistemaCadastro/src/Devs2Blu.ProjetosAula.SistemaCadastro.Forms/Form1.cs
@@ -0,0 +1,124 @@
+﻿using Devs2Blu.ProjetosAula.SistemaCadastro.Forms.Data;
+using Devs2Blu.ProjetosAula.SistemaCadastro.Models.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Devs2Blu.ProjetosAula.SistemaCadastro.Forms
+{
+    public partial class Form1 : Form
+    {
+        public MySqlConnection Conn { get; set; }
+        public ConvenioRepository ConvenioRepository = new ConvenioRepository();
+        public PacienteRepository PacienteRepository = new PacienteRepository();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Methods
+        private void PopulaComboConvenio()
+        {
+            var listConvenios = ConvenioRepository.FetchAll();
+            cboConvenio.DataSource = new BindingSource(listConvenios, null);
+            cboConvenio.DisplayMember = "nome";
+            cboConvenio.ValueMember = "id";
+        }
+
+        private void PopulaDataGridPessoa()
+        {
+            var listPessoas = PacienteRepository.GetPessoas();
+            gridPacientes.DataSource = new BindingSource(listPessoas, null);
+        }
+        private bool ValidaFormCadastro()
+        {
+            if (txtNome.Text.Equals(""))
+                return false;
+            if (txtCGCCPF.Text.Equals(""))
+                return false;
+            /*if (cboConvenio.SelectedIndex == -1)
+                return false;
+            if (mskCEP.Text.Equals(""))
+                return false;
+            if (cboUF.SelectedIndex == -1)
+                return false;
+            if (txtCidade.Text.Equals(""))
+                return false;
+            if (txtRua.Text.Equals(""))
+                return false;
+            if (txtNumero.Text.Equals(""))
+                return false;
+            if (txtBairro.Text.Equals(""))
+                return false;*/
+
+            return true;
+        }
+        #endregion
+
+        #region Events
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region TesteConexao
+            /*Conn = ConnectionMySQL.GetConnection();
+
+            if (Conn.State == ConnectionState.Open)
+            {
+                MessageBox.Show("Conexão efetuada com sucesso!", "Conexão ao MySQL", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                Conn.Close();
+            }*/
+            #endregion
+            PopulaComboConvenio();
+            PopulaDataGridPessoa();
+        }
+
+        private void rdFisica_CheckedChanged(object sender, EventArgs e)
+        {
+            lblCGCCPF.Text = "CPF";
+            lblCGCCPF.Visible = true;
+        }
+
+        private void rdJuridica_CheckedChanged(object sender, EventArgs e)
+        {
+            lblCGCCPF.Text = "CNPJ";
+            lblCGCCPF.Visible = true;
+        }
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (ValidaFormCadastro())
+            {
+                Paciente paciente = new Paciente();
+                paciente.Pessoa.Nome = txtNome.Text;
+                paciente.Pessoa.CGCCPF = txtCGCCPF.Text.Replace(',','.');
+                paciente.Convenio.Id = (int) cboConvenio.SelectedValue;
+                Endereco endereco = new Endereco();
+                endereco.CEP = mskCEP.Text;
+                endereco.Rua = txtRua.Text;
+                endereco.Numero = Convert.ToInt32(txtNumero.Text);
+                endereco.Bairro = txtBairro.Text;
+                endereco.Cidade = txtCidade.Text;
+                endereco.UF = (string)cboUF.SelectedValue;
+
+                var pacienteResult = PacienteRepository.Save(paciente, endereco);
+                if (pacienteResult.Pessoa.Id > 0)
+                {
+                    MessageBox.Show($"Pessoa {paciente.Pessoa.Id} - {paciente.Pessoa.Nome} salva com sucesso!", "Adicionar Pessoa", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    PopulaDataGridPessoa();
+                }
+            }
+        }
+        #endregion
+
+        private void gpCadPaciente_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
